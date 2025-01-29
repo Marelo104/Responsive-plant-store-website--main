e8e826499acc5c18ef8dbe694644d8913c25d1bf
Marelo104 <yanninckaltay@gmail.com>
2025-01-29 07:20:12 +0100

    initial commit

--- script.js
+++ script.js
@@ -39,31 +39,57 @@ window.addEventListener('scroll', scrollUp);
     });
  })
 
- let cardOpen  = document.querySelectorAll('.FAQs-card-content')
  let clickCard = document.querySelectorAll('.FAQs-card');
+ 
  clickCard.forEach((card) => { 
     const toggleCard  = card.querySelector('.FAQs-card-content')
     card.addEventListener('click', () => {
-        let items = document.querySelector('.content-item');
-        if(items && items !== card){
-            items.classList.remove('content-item');
-            toggleCard.removeAttribute('style');
-            console.log(items.removeAttribute('style'))
-            console.log(items);
-            console.log(card);
-
-        }
         if(card.classList.contains('content-item')){
-            card.classList.remove('content-item');
-            toggleCard.removeAttribute('style');
+            handleToggle(clickCard)
         }else{
+            handleToggle(clickCard)
             card.classList.add('content-item');   
             toggleCard.style.height = toggleCard.scrollHeight + 'px'
         }
     });
  });
 
+ const handleToggle = (clickCard) => {
+    clickCard.forEach((item) => {
+        item.querySelector('.FAQs-card-content').removeAttribute('style')
+        item.classList.remove('content-item');
+    });
+ };
 
+ const sections = document.querySelectorAll('section[id]')
+ console.log(sections)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+        console.log(sectionId)
+        
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// let items = document.querySelector('.content-item');
+// if(items && items !== card){
+//     let openItem = items.querySelector('.FAQs-card-content');
+//     items.classList.remove('content-item');
+//     openItem.removeAttribute('style');
+//     console.log(items);
+//     console.log(card);
+// }
 
 
 // clickCard.forEach((item) =>{
@@ -92,6 +118,21 @@ window.addEventListener('scroll', scrollUp);
 //     }
 
 // }
+
+const sr = ScrollReveal({
+   origin: 'top',
+   distance: '60px',
+   duration: 2500,
+   delay: 400,
+   // reset: true
+})
+
+sr.reveal(`.home-data`)
+sr.reveal(`.home-img`, {delay: 500})
+sr.reveal(`.home-social`, {delay: 600})
+sr.reveal(`.about-img, .contact-data`,{origin: 'left'})
+sr.reveal(`.about-data, .contact-form`,{origin: 'right'})
+sr.reveal(`.step-card, .product-card, .FAQs-card-container, footer`,{interval: 100})
 
 
 
